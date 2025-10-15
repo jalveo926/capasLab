251f074870db210a70d9423d2a98b779808289a9
jalveo926 <jesusalveo445@gmail.com>
2025-10-14 20:33:20 -0500

    Move project files to Laboratorio5-DSIV directory
    All source, resource, and configuration files from capasLab and capasLab-main/Laboratorio5-DSIV were moved and renamed to the Laboratorio5-DSIV directory. This consolidates the project structure and removes duplicate and obsolete files from the previous locations.

---
+++ Laboratorio5-DSIV/FormReserva.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio5_DSIV
+{
+    public partial class FormReserva : Form
+    {
+        public FormReserva()
+        {
+            InitializeComponent();
+            NUDPlatino.ValueChanged += numericUpDown_ValueChanged;
+            NUDVIP.ValueChanged += numericUpDown_ValueChanged;
+            NUDGeneral.ValueChanged += numericUpDown_ValueChanged;
+
+        }
+
+        private void numericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown actual = sender as NumericUpDown;
+            int total = (int)(NUDPlatino.Value + NUDVIP.Value + NUDGeneral.Value);
+            if (total > 5)
+            {
+                int exceso = total - 5;
+                actual.Value -= exceso;
+            }
+        }
+
+        private void bttnConfirm_Click(object sender, EventArgs e)
+        {
+            FormResumen compraForm = new FormResumen();
+            compraForm.Show();
+            this.Hide();
+        }
+
+        private void bttnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
